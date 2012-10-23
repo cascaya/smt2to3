39d21bedc2f0edea3be0c840894a402e858b99d5
Anja Siek <ja.sie.kan@googlemail.com>
2012-10-23 23:07:05 +0200

    replace with real shell script

--- tplsmarty2to3.sh
+++ tplsmarty2to3.sh
@@ -1,93 +1,58 @@
-require 'rubygems'
+#!/bin/sh
+
+
+usage()
+{
+    {
+        echo "$(basename -- $0 ) -o \$ORIGINAL -n \$NEW"
+        echo "  o: ORIGINAL-DIR /path/to/templates/ -> default == templates"
+        echo "  n: NEW-DIR /path/to/templatesSmarty3/ -> will be create if not exists"
+
+    } 2>&1
+
+}
+
+
+ORGDIR="templates"
+NEWDIR=
+
+#handel incomming vars
+while getopts hon: OPT; do
+    case "$OPT" in
+        h) usage; exit 0;;
+        o) ORGDIR=$OPTARG;;
+        n) NEWDIR=$OPTARG;;
+    esac
+done
+
+shift $(( $OPTIND - 1 ))
+
+
+#check vars
+if [ -z "$ORGDIR" ] || [ ! -d "$ORGDIR" ]; then
+    echo "ORIGINALGDIR can not be empty or is no Directory";
+    usage; exit 1;
+elif [ -z "$NEWDIR" ]; then
+    echo "NEWDIR can not be empty";
+    usage; exit 1;
+fi
  
-
-class Smarty2To3Converter
-
-    @processor
-    @file
-
-    def initialize(filename)
-        if File.readable? filename then @file = filename end
-        @processor = Processor.new
-    end
-
-    def run
-        File.open(@file, 'r').readlines.each do |line|
-            @processor.run line
-        end
-    end
-
-end
+# create new dir if not exists
+if [ ! -d  "$NEWDIR" ]; then 
+    mkdir $NEWDIR;
+fi
 
 
-class Processor
 
-    @isLitteral
+for i in $(find $ORGDIR -name "*.tpl"); do 
+    NEWFILE=$(echo $i |sed -e "s/${ORGDIR}/${NEWDIR}/g"); 
+    DIRS=$(dirname $NEWFILE);
+    
+    if  [ ! -d $DIRS ]; then
+        mkdir -p $DIRS;
+    fi
+    
+    ruby tplsmarty2to3.rb $i >$NEWFILE;
 
-    def initialize()
-        @isLitteral = false
-    end
- 
-    def run line
+done;
 
-        if @isLitteral || line.empty?
-           puts line 
-           return
-        end
-
-        # replace first all spacess so that we can search for that pattern
-        line = line.gsub(/\{\s+\literal\s+\}/,"{literal}")
-        line = line.gsub(/\{\s+\/literal\s+\}/,"{/literal}")
-
-        # start logic
-        puts hasLitteralStart(line)
-
-    end
-
-    def hasLitteralStart line
-
-        if (!line.empty? && line.index("{literal}"))
-        
-            @isLitteral = true
-            splitline = line.split("{literal}" , 2)
-            
-
-            splitline[0] = searchAndReplace splitline[0]
-            splitline[1] = hasLitteralEnd splitline[1] 
-            
-            return splitline * "{literal}"
-        end
-
-        return hasLitteralEnd line
-
-    end
-
-    def hasLitteralEnd line
-        if (!line.empty? && line.index("{/literal}"))
-           
-            @isLitteral = false
-            
-            splitline = line.split("{/literal}" , 2)
-
-            splitline[1] = hasLitteralStart splitline[1]
-            
-            return splitline * "{/literal}"
-        end
-
-        return searchAndReplace line
-
-    end
-
-    def searchAndReplace line
-        line = line.gsub(/\{\s+/,"{")
-        line = line.gsub(/\s+\}/, "}")
-        line = line.gsub(/(\$\S+)\|isset/, 'isset(\1)')
-        line = line.gsub(/value=(\S+\.tpl)\}/, 'value="\1"}')
-
-        return line
-    end
-
-  
-end
-
-Smarty2To3Converter.new(ARGV[0]).run
